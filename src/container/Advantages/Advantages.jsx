83ae45bd7c93c89a0356d5ae06889c4a31b77827
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2023-01-04 16:09:05 +0600

    advantages workspace setup

--- src/container/Advantages/Advantages.jsx
+++ src/container/Advantages/Advantages.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
 import './Advantages.scss';
 
+import { images } from '../../constrants/index';
+
+
 export default function Advantages() {
+  const advantageObj = [
+    {
+      title: "Figma",
+      img: images.htmlIcon
+    },
+    {
+      title: "Figma",
+      img: images.htmlIcon
+    },
+    {
+      title: "Figma",
+      img: images.htmlIcon
+    },
+    {
+      title: "Figma",
+      img: images.htmlIcon
+    },
+    {
+      title: "Figma",
+      img: images.htmlIcon
+    },
+    {
+      title: "Figma",
+      img: images.htmlIcon
+    }
+  ]
+  // const html = images.htmlIcon;
+  // const elementor = images.elementor;
+  // const wordpress = images.wordpress;
+  // const advImg = [html, elementor, wordpress]
   return (
-    <div>Advantages</div>
+    <div>
+
+      <h1>Advantages</h1>
+
+      {advantageObj.map((item, index) =>
+        <div key={index} className="advItems">
+            <h1>{item.title}</h1>
+            <img src={item.img} alt="" />
+        </div>
+      )}
+
+    </div>
   )
 }
