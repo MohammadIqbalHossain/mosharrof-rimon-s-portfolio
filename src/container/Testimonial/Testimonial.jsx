73f31b422a356bff45d599a3c94ec6617edb7092
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2023-01-05 15:44:06 +0600

    testimonial added

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -3,6 +3,7 @@ import './Testimonial.scss';
 
 import { Swiper } from "swiper";
 
+
 // Import Swiper styles
 import 'swiper/css';
 
@@ -23,46 +24,73 @@ function Testimonial() {
 
 
   return (
-    <div class="blog-slider">
-      <div class="blog-slider__wrp swiper-wrapper">
-        <div class="blog-slider__item swiper-slide">
-          <div class="blog-slider__img">
-
-            <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp" alt="" />
+    <div class="header">
+      <div class="container">
+        <div class="slider">
+          <div class="testimonal" id="testimonal-1">
+            <div class="img">
+              <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" class="user" />
+            </div>
+            <div class="item-content">
+              <h3>Lorem name</h3>
+              <div class="twitter"><small>@loremname <i class="fab fa-twitter"></i></small></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nemo dolorem possimus
+                delectus sint modi! Minima omnis exercitationem nemo ea.</p>
+            </div>
           </div>
-          <div class="blog-slider__content">
-            <span class="blog-slider__code">26 December 2019</span>
-            <div class="blog-slider__title">Lorem Ipsum Dolor</div>
-            <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
-            <a href="#" class="blog-slider__button">READ MORE</a>
+          <div class="testimonal" id="testimonal-2">
+            <div class="img">
+              <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" class="user" />
+            </div>
+            <div class="item-content">
+              <h3>Lorem name</h3>
+              <div class="twitter"><small>@loremname <i class="fab fa-twitter"></i></small></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nemo dolorem possimus
+                delectus sint modi! Minima omnis exercitationem nemo ea.</p>
+            </div>
+          </div>
+          <div class="testimonal" id="testimonal-3">
+            <div class="img">
+              <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" class="user" />
+            </div>
+            <div class="item-content">
+              <h3>Lorem name</h3>
+              <div class="twitter"><small>@loremname <i class="fab fa-twitter"></i></small></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nemo dolorem possimus
+                delectus sint modi! Minima omnis exercitationem nemo ea.</p>
+            </div>
+          </div>
+          <div class="testimonal" id="testimonal-4">
+            <div class="img">
+              <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" class="user" />
+            </div>
+            <div class="item-content">
+              <h3>Lorem name</h3>
+              <div class="twitter"><small>@loremname <i class="fab fa-twitter"></i></small></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nemo dolorem possimus
+                delectus sint modi! Minima omnis exercitationem nemo ea.</p>
+            </div>
+          </div>
+          <div class="testimonal" id="testimonal-5">
+            <div class="img">
+              <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" class="user" />
+            </div>
+            <div class="item-content">
+              <h3>Lorem name</h3>
+              <div class="twitter"><small>@loremname <i class="fab fa-twitter"></i></small></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nemo dolorem possimus
+                delectus sint modi! Minima omnis exercitationem nemo ea.</p>
+            </div>
           </div>
         </div>
-        <div class="blog-slider__item swiper-slide">
-          <div class="blog-slider__img">
-            <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/jason-leung-798979-unsplash.webp" alt="" />
-          </div>
-          <div class="blog-slider__content">
-            <span class="blog-slider__code">26 December 2019</span>
-            <div class="blog-slider__title">Lorem Ipsum Dolor2</div>
-            <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
-            <a href="#" class="blog-slider__button">READ MORE</a>
-          </div>
+        <div class="slider-btn">
+          <a href="#testimonal-1"><span class="dot"></span></a>
+          <a href="#testimonal-2"><span class="dot"></span></a>
+          <a href="#testimonal-3"><span class="dot"></span></a>
+          <a href="#testimonal-4"><span class="dot"></span></a>
+          <a href="#testimonal-5"><span class="dot"></span></a>
         </div>
-
-        <div class="blog-slider__item swiper-slide">
-          <div class="blog-slider__img">
-            <img src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/alessandro-capuzzi-799180-unsplash.webp" alt="" />
-          </div>
-          <div class="blog-slider__content">
-            <span class="blog-slider__code">26 December 2019</span>
-            <div class="blog-slider__title">Lorem Ipsum Dolor</div>
-            <div class="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
-            <a href="#" class="blog-slider__button">READ MORE</a>
-          </div>
-        </div>
-
       </div>
-      <div class="blog-slider__pagination"></div>
     </div>
   )
 }
