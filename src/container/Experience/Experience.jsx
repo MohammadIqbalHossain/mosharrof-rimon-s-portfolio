b72fdb4260294c5131c87994727fda037ea52b15
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2023-01-05 14:10:03 +0600

    experaince section added

--- src/container/Experience/Experience.jsx
+++ src/container/Experience/Experience.jsx
@@ -2,7 +2,56 @@ import React from 'react'
 import './Experience.scss';
 
 export default function Experience() {
+
+  const expData = [
+    {
+      year: "2016",
+      institute: "RMIT UNIVERSITY",
+      degree: "Bachelor Degree of Design"
+    },
+    {
+      year: "2016",
+      institute: "RMIT UNIVERSITY",
+      degree: "Bachelor Degree of Design"
+    },
+    {
+      year: "2016",
+      institute: "RMIT UNIVERSITY",
+      degree: "Bachelor Degree of Design"
+    },
+    {
+      year: "2016",
+      institute: "RMIT UNIVERSITY",
+      degree: "Bachelor Degree of Design"
+    },
+    {
+      year: "2016",
+      institute: "RMIT UNIVERSITY",
+      degree: "Bachelor Degree of Design"
+    },
+  ]
+
   return (
-    <div>Experience</div>
+    <div>
+         <h1>Education & Experience</h1>
+      {expData.map((item, index) =>
+        <div>
+          <div key={index} className="expContainer">
+            <div className="degree">
+              <small>{item.year}</small>
+              <span>{item.degree}</span>
+            </div>
+            <div className="institute">
+              <span>{item.institute}</span>
+            </div>
+          </div>
+          <hr className="hr-line"/>
+        </div>
+
+      )}
+
+
+
+    </div>
   )
 }
